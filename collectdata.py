4ee05cedac0cb34e2f38aa7acef8de93d5277470
Afrazinator <afrazarif@hotmail.com>
2020-03-24 16:45:40 -0700

    Working extraction on page 1

--- collectdata.py
+++ collectdata.py
@@ -1,9 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+from urllib.parse import urljoin
 
+fdapage = requests.get("https://www.fda.gov/news-events/fda-newsroom/press-announcements")
 page = requests.get("https://www.fda.gov/news-events/press-announcements/fda-approves-first-treatment-group-progressive-interstitial-lung-diseases")
 
-soup = BeautifulSoup(page.content,'html.parser')
+soup = BeautifulSoup(fdapage.content,'lxml')
+urls = set()
+
+for i in soup.find_all('a',href=re.compile(r'(?<=approves).*')):
+    url = i.get('href')
+    abs_url = urljoin(fdapage.url, url)
+    urls.add(abs_url)
+print(urls)
 
 text = soup.find_all('p')
 
