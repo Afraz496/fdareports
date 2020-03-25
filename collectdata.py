594da23473fed66ad9894c847f2759742735f0d5
Afraz Khan <afrazarif@hotmail.com>
2020-03-25 16:47:31 -0700

    Extracted all 2018 URLS

--- collectdata.py
+++ collectdata.py
@@ -3,19 +3,16 @@ from bs4 import BeautifulSoup
 import re
 from urllib.parse import urljoin
 
-fdapage = requests.get("https://www.fda.gov/news-events/fda-newsroom/press-announcements")
-page = requests.get("https://www.fda.gov/news-events/press-announcements/fda-approves-first-treatment-group-progressive-interstitial-lung-diseases")
+for i in range(0,60):
+    fdapage = requests.get("https://www.fda.gov/news-events/fda-newsroom/press-announcements?page=" + str(i))
+    page = requests.get("https://www.fda.gov/news-events/press-announcements/fda-approves-first-treatment-group-progressive-interstitial-lung-diseases")
 
-soup = BeautifulSoup(fdapage.content,'lxml')
-urls = set()
+    soup = BeautifulSoup(fdapage.content,'lxml')
+    urls = set()
 
-for i in soup.find_all('a',href=re.compile(r'(?<=approves).*')):
-    url = i.get('href')
-    abs_url = urljoin(fdapage.url, url)
-    urls.add(abs_url)
-print(urls)
-
-text = soup.find_all('p')
-
-for t in text:
-    print(t.text)
+    for i in soup.find_all('a',href=re.compile(r'(?<=approves).*')):
+        url = i.get('href')
+        abs_url = urljoin(fdapage.url, url)
+        urls.add(abs_url)
+        if urls is not set():
+            print(urls)
